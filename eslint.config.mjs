d34ba1a2eb329c857fe5ec81393da10187072c5c
seok-hyung <korea970223@naver.com>
2025-04-01 00:06:18 +0900

    🐛 TypeScript Parser, eslint에 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,39 @@
 import js from '@eslint/js';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 
 export default [
-	js.configs.recommended,
-	{
-		files: ['**/*.{js,jsx,ts,tsx}'],
-		plugins: {
-			react,
-			'react-hooks': reactHooks,
-		},
-		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			parserOptions: {
-				ecmaFeatures: {
-					jsx: true,
-				},
-			},
-		},
-		rules: {
-			'no-unused-vars': 'warn',
-			'no-console': 'warn',
-			'react/react-in-jsx-scope': 'off',
-			'react-hooks/rules-of-hooks': 'error',
-			'react-hooks/exhaustive-deps': 'warn',
-		},
-		settings: {
-			react: {
-				version: 'detect',
-			},
-		},
-	}
-]; 
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': typescript
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+];
