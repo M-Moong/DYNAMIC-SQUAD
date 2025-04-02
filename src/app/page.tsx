0a2f368343817e16caab2cd698c88d7aba4e847a
Moong <tmdals9154@gmail.com>
2025-04-02 20:57:41 +0900

    chore: test 커밋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@
 // import Section4 from './(section4)/page';
 // import Section5 from './(section5)/page';
 
+import Section1 from './(section1)/page';
+
 export default function Home() {
   return (
     <>
@@ -11,8 +13,8 @@ export default function Home() {
         <span className="font-bold text-blue-400">Intro</span> Page
         <span className="text-blue-300">.</span>
       </div>
-      {/* <Section1 />
-      <Section2 />
+      <Section1 />
+      {/* <Section2 />
       <Section3 />
       <Section4 />
       <Section5 /> */}
