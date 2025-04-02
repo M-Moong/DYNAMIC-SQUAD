e71c74c6fa800337d8c1f99f6e7aa4ec96dab3c6
Moong <tmdals9154@gmail.com>
2025-04-02 20:37:27 +0900

    chore: dummy 파일 생성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,35 +5,35 @@ import typescript from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
 
 export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      '@typescript-eslint': typescript
-    },
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parser: typescriptParser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
-      }
-    },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'warn',
-      'react/react-in-jsx-scope': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn'
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    }
-  }
+	js.configs.recommended,
+	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		plugins: {
+			react,
+			'react-hooks': reactHooks,
+			'@typescript-eslint': typescript
+		},
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			parser: typescriptParser,
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true
+				}
+			}
+		},
+		rules: {
+			'no-unused-vars': 'warn',
+			'no-console': 'warn',
+			'react/react-in-jsx-scope': 'off',
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'warn'
+		},
+		settings: {
+			react: {
+				version: 'detect'
+			}
+		}
+	}
 ];
