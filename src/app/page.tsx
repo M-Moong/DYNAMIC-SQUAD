56c47e5692bb2d3c47ef37d16ea756758c6d63af
soheeyeo <sohee407@gmail.com>
2025-04-02 23:51:12 +0900

    fix: page 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-// import Section1 from './(section1)/page';
-// import Section2 from './(section2)/page';
-// import Section3 from './(section3)/page';
-// import Section4 from './(section4)/page';
-// import Section5 from './(section5)/page';
-
-import Section1 from './(section1)/page';
-
 export default function Home() {
   return (
     <>
@@ -13,11 +5,6 @@ export default function Home() {
         <span className="font-bold text-blue-400">Intro</span> Page
         <span className="text-blue-300">.</span>
       </div>
-      <Section1 />
-      {/* <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 /> */}
     </>
   );
 }
