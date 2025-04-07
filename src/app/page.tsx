2de14177d5121bd0e309f7662ff8242052a1860e
seok-hyung <korea970223@naver.com>
2025-04-08 01:35:22 +0900

    feat: 스크롤 기반 캐러셀 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-// import Section1 from './(section1)/page';
-// import Section2 from './(section2)/page';
-// import Section3 from './(section3)/page';
-// import Section4 from './(section4)/page';
-// import Section5 from './(section5)/page';
+import ImageCarousel from './components/ImageCarousel';
 
 export default function Home() {
   return (
@@ -11,11 +7,7 @@ export default function Home() {
         <span className="font-bold text-blue-400">Intro</span> Page
         <span className="text-blue-300">.</span>
       </div>
-      {/* <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 /> */}
+      <ImageCarousel />
     </>
   );
 }
